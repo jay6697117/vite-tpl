f5d97fc37c5e67475da862a6a0ca185d1c465bc3
steve zhang <871506263@qq.com>
2022-09-24 22:35:56 +0800

    提交代码于: 2022-09-24 22:35:56

--- src/demo/demo-6.ts
+++ src/demo/demo-6.ts
@@ -1,10 +1,19 @@
 export {};
 
 // 1.数组的类型
+
 // 1.1「类型 + 方括号」表示法
-let fibonacci: number[] = [1, 1, 2, 3, 5];
-let fibonacci1: string[] = ['a', 'b', 'c', 'd', 'e'];
-// let fibonacci2: number[] = [1, '1', 2, 3, 5];
-console.log('fibonacci', fibonacci);
-console.log('fibonacci1', fibonacci1);
-// console.log('fibonacci2', fibonacci2);
+// let fibonacci: number[] = [1, 1, 2, 3, 5];
+// let fibonacci1: string[] = ['a', 'b', 'c', 'd', 'e'];
+// // let fibonacci2: number[] = [1, '1', 2, 3, 5];
+// console.log('fibonacci', fibonacci);
+// console.log('fibonacci1', fibonacci1);
+// // console.log('fibonacci2', fibonacci2);
+// let fibonacci: number[] = [1, 1, 2, 3, 5];
+// // fibonacci.push('8');
+// fibonacci.push(8);
+// console.log('fibonacci', fibonacci);
+
+// 1.2 数组泛型
+let arr: Array<number> = [1, 2, 3, 4, 5];
+console.log('arr', arr);
